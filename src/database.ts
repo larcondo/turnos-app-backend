60a57575b01c296a66e742f6446122f60cc8adf8
larcondo <larcondo90@gmail.com>
2024-05-19 19:40:07 -0300

    change database default path

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,12 @@
 import sqlite3 from 'sqlite3';
+import path from 'path';
 
 sqlite3.verbose();
 
-const db = new sqlite3.Database('./src/turnos.db', (err) => {
+const databasePath = path.join(__dirname, '..', 'databases');
+const DATABASE = 'turnos.db';
+
+const db = new sqlite3.Database(path.join(databasePath, DATABASE), (err) => {
   if (err) {
     console.log(err.message);
   } else {
