e6fdad2cda5aefff81c88fb1479459c8bdfec62a
larcondo <larcondo90@gmail.com>
2024-05-20 17:38:50 -0300

    add rol field to usuarios table

--- src/queries/usuarios.ts
+++ src/queries/usuarios.ts
@@ -2,7 +2,8 @@ export const CREATE_USUARIOS_TABLE =
 `CREATE TABLE IF NOT EXISTS usuarios(
   id TEXT PRIMARY KEY NOT NULL,
   email TEXT NOT NULL UNIQUE,
-  nombre TEXT NOT NULL
+  nombre TEXT NOT NULL,
+  rol TEXT NOT NULL DEFAULT 'client'
 );`;
 
 export const ALL_USERS: string = 'SELECT * FROM usuarios';
